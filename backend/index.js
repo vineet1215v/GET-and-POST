cd63408e5ed8cf57e44d36e5ac9c1d9a5ea49d03
vineet1215v <vineeth1215v@gmail.com>
2026-03-13 23:58:57 +0530

    index.js modified

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const app = express();
-const port = 8000;
+    const express = require("express");
+    const app = express();
+    const port = 8000;
 
-//Parse POST request data(middlewares)
-app.use(express.urlencoded({extended:true}));
+    //Parse POST request data(middlewares)
+    app.use(express.urlencoded({extended:true}));
 
-//if data send in json format from frontend(postman or thunderclient)
-app.use(express.json());
+    //if data send in json format from frontend(postman or thunderclient)
+    app.use(express.json());
+    
+    app.get("/register",(req,res)=>{
+        let {user, password} = req.query;
+        res.send(`This is the GET Response,welcome ${user}`);
+    });
 
-app.get("/register",(req,res)=>{
-    let {user, password} = req.query;
-    res.send(`This is the GET Response,welcome ${user}`);
-});
-
-app.post("/register",(req,res)=>{
-    // console.log(req.body);
-    let {user, password} = req.body;
-    res.send(`This is the POST Response,welcome ${user}`);
-});
+    app.post("/register",(req,res)=>{
+        // console.log(req.body);
+        let {user, password} = req.body;
+        res.send(`This is the POST Response,welcome ${user}`);
+    });
 
 
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+    app.listen(port,()=>{
+         console.log(`Server is running on port ${port}`);
+    })
